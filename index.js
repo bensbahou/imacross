0e62ccdd8abc78acd6a1110d5914e52bcbe3b4aa
bensbahou <younesbensbahou@gmail.com>
2023-09-26 00:06:51 +0100

    github call localhost

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hiiiiiiiiii");
+(function () {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://localhost:3000/static/index.js"; // Replace with your localhost URL
+  document.head.appendChild(script);
+})();
